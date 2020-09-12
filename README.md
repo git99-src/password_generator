f957815309a3b843d080c8fd382e5736ad5dcf1e
git99.src <cpu.al2@gmail.com>
2020-09-11 22:45:43 -0700

    Final commit.  Completed logic in javascript to handle all char types

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # password_generator
-This will present a site which suggests a password based on criteria presented to the user
+
+
+## Description 
+
+This will present a site which suggests a password based on criteria presented to the user.  The user will be prompted to enter a password length minimum of 8 and maximum of 128.  The user will then be prompted for what type of characters they would like to include in the password - upper case, lower case, numbers and symbols.  Based on the user requirements, a random password will be generated and presented to the user.
+
+https://git99-src.github.io/password_generator/
+
+
+## Installation
+
+This site is installed to the Github repository which includes HTML pages, style sheet and images. Javascript was used extensively to provide the application logic.   
+
+
+## Usage 
+
+Code can be used for collaborative projects with permission.
--- assets/js/script.js
+++ assets/js/script.js
@@ -3,26 +3,25 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  //var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  let randomArr = [" "];
-  let selectType = ["l","u","n","s"];
+  let randomArr = [""];
   let lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
   let upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
   let passNum = ["0","1","2","3","4","5","6","7","8","9"];
   let symbols = [" ", "!", '"', "#", "$", "%","&","'" ,"(", ")", "*","+",",","-", ".", ":", ";","<", "=",">", "?", "@", "[", "^","`", "{", "|", "}", "~" ];
-  
-  var lengthPassword = prompt("Please enter the length of the password - must be at least 8 and no more than 128 and include a letter (upper and lower) and special character");
+  var lengthPassword = prompt("Please enter the length for the password - must be at least 8 and no more than 128.");
   var newPassword = " ";
 
 
-  // test if user entered a length
-  if (lengthPassword >= 8 && lengthPassword <= 128) {
+  // be sure user entered a password length within the range
+  if (lengthPassword < 8 || lengthPassword > 128) {
      
     //not valid password length entered
     alert("Password length must be at least 8 and no longer than 128.");
     
+  } else {
    
     // confirm if user wishes to use lowercase and load selection array
     if (confirm("Should the password include lowercase letters?")){
@@ -55,84 +54,78 @@ function writePassword() {
     };
 
     // loop to create password by user entered length
-  
-    for (i=0;i < lengthPassword;i++) {
-
-        //check 1- 4 iterations to ensure one of each type is satisfied
-        // // // if i = 1 add uppercase to newPassword
-        // if (i = 1 && needUpper) {
-        //     add lowercase to newPassword
-        // }
-        // // if i = 2 add number to newPassword
-        // if (i = 2 && needNumber) {
-        //     add lowercase to newPassword
-        // }
-        // // if i = 3 add symbol to newPassword
-        // if (i = 2 && needSymbol) {
-        //     add lowercase to newPassword
-        // }
+   
+    for (i = 0; i < lengthPassword; i++) {
         
-        newPassword = newPassword+(random_item(randomArr));
-        console.log(newPassword)
-    }   
+        // following just ensures we get one of each type selected
+        
+        if (i === 0 && needLower) {
+         
+        // add lowercase to newPassword
+        newPassword = newPassword+(getRandomLower(lowerCase));
+        
+        } else if (i === 1 && needUpper) {
+            
+        // add uppercase to newPassword
+        newPassword = newPassword+(getRandomUpper(upperCase));    
+        } else if (i === 2 && needNumber) {
+            
+        // add number to newPassword
+        newPassword = newPassword+(getRandomNumber(passNum));       
+        } else if (i === 3 && needSymbol) {
+            
+        // add symbol to newPassword
+        newPassword = newPassword+(getRandomSymbol(symbols));       
+        } else {
+
+        // load rest of password with completely random characters
+            newPassword = newPassword+(randomCharacter(randomArr));
+        };
+    };  
     
 
     password = newPassword;
-};
+    };
   //present the generated password in the password text 
   passwordText.value = password;
   };
 
 
-
-    // function to generate password
-    function generatePassword () {
-        return ;
-    };
-
-function random_item(randomArr){
+// retrieve a random character from array built with all char type selections
+function randomCharacter(randomArr){
   
-return randomArr[Math.floor(Math.random()*(randomArr.length-1))];
+return randomArr[Math.floor(Math.random()*(randomArr.length))];
      
 };
 
-// // function to generate random lowercase
-// function getRandomLower () {
-//     Math.floor(Math.random() * 25);
-// };
+// function to generate random lowercase
+function getRandomLower(lowerCase){
+  
+    return lowerCase[Math.floor(Math.random()*(lowerCase.length))];
+         
+};
+// function to generate random uppercase
+function getRandomUpper(upperCase){
+  
+    return upperCase[Math.floor(Math.random()*(upperCase.length))];
+         
+};
 
-// reuse to generate random uppercase
-// function getRandomUpper () {
-//     Math.floor(Math.random() * 25);
-//  }
+// function to generate random numbers
+function getRandomNumber(passNum){
+  
+    return passNum[Math.floor(Math.random()*(passNum.length))];
+         
+};
 
-// reuse to generate random symbol
-// function getRandomSymbol () {
-//     Math.floor(Math.random() * 10);
-// }
-
-// reuse to generate random integer
-// function getRandomInteger () {
-//     Math.floor(Math.random() * 10);
-// }
+// function to generate random symbol
+function getRandomSymbol(symbols){
+  
+    return symbols[Math.floor(Math.random()*(symbols.length))];
+         
+};
 
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-
-// GIVEN I need a new, secure password
-// WHEN I click the button to generate a password
-// THEN I am presented with a series of prompts for password criteria
-// WHEN prompted for password criteria
-// THEN I select which criteria to include in the password
-// WHEN prompted for the length of the password
-// THEN I choose a length of at least 8 characters and no more than 128 characters
-// WHEN prompted for character types to include in the password
-// THEN I choose lowercase, uppercase, numeric, and/or special characters
-// WHEN I answer each prompt
-// THEN my input should be validated and at least one character type should be selected
-// WHEN all prompts are answered
-// THEN a password is generated that matches the selected criteria
-// WHEN the password is generated
-// THEN the password is either displayed in an alert or written to the page
